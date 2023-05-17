d64a79d4a7c260833642b76c48c449f75f73abf8
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-05-16 19:27:20 -0700

    commit tree 2a77155d05b919763aa649003f107ce11a3178db

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.delete(" ").split(/[\W]/).count
   end
 end
+
+pp "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
+
